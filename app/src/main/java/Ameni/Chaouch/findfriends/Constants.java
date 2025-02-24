17a42c11c4d3bbd6a942f63c195153f8a27b1c09
Ameni Chaouch <chawechemeni987@gmail.com>
2025-02-24 18:44:21 +0100

    commit

---
+++ app/src/main/java/Ameni/Chaouch/findfriends/Constants.java
@@ -0,0 +1,8 @@
+package Ameni.Chaouch.findfriends;
+
+public class Constants {
+    public static  final String MSG_SendMePosition="Envoyer moi votre position";
+    public static boolean GPS_SMS_PERMISSION_Stats=false;
+    public  static final  String MSG_MAPOSITION="Friends,Ma position est";
+
+}
---
+++ app/src/main/java/Ameni/Chaouch/findfriends/ui/home/HomeFragment.java
@@ -0,0 +1,51 @@
+package Ameni.Chaouch.findfriends.ui.home;
+
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import Ameni.Chaouch.findfriends.Constants;
+import Ameni.Chaouch.findfriends.databinding.FragmentHomeBinding;
+
+public class HomeFragment extends Fragment {
+
+    private FragmentHomeBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        binding.btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String number=binding.edPhone.getText().toString();
+                //envoie sms
+                if (Constants.GPS_SMS_PERMISSION_Stats)
+                {
+                    SmsManager manager=SmsManager.getDefault();//sim par défaut
+                    manager.sendTextMessage(number,null, Constants.MSG_SendMePosition,null,null);
+                }
+                else Toast.makeText(getActivity(), "Autorisation non accordée", Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+        return root;
+    }
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
